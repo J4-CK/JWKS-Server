75d67c0cacdf98e852f7751c558869e2f189e902
J4-CK <142830650+J4-CK@users.noreply.github.com>
2024-03-02 22:09:05 -0600

    Update jwks_server.py
    trimmed fat from debugging

--- jwks_server.py
+++ jwks_server.py
@@ -6,12 +6,10 @@ from cryptography.hazmat.primitives import serialization
 import jwt 
 import os
 
-
 app = Flask(__name__)
 
 #determine a better key
 SECRET_KEY = os.urandom(32)  # 32 bytes (256 bits) is a common size for JWT secret keys
-
 
 import time
 
@@ -47,34 +45,7 @@ def generate_rsa_key():
     
     return private_pem, public_pem, kid, expiration_time
 
-'''replacing RSA
-#Function to generate RSA key pair
-def generate_rsa_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    
-    #Serialize private key to PEM format
-    private_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-    
-    #Get public key
-    public_key = private_key.public_key()
-    
-    #Serialize public key to PEM format
-    public_pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    
-    return private_pem, public_pem
 
-'''
 #Fix to make expiration dynamic instead of static
 def refresh_sample_key():
     global sample_key
@@ -86,31 +57,9 @@ def refresh_sample_key():
         "e": "AQAB",  #Exponent (fixed value for RSA)
         "exp": int(time.time()) + 3600  #Set an initial expiration time (1 hour)
     }
-'''
 
-#Function to refresh keys
-#these are still test values and must be changed for real RSA public key info
-def refresh_keys():
-    global private_key, public_key, sample_key
-    private_key, public_key = generate_rsa_key()
-    sample_key = {
-        "kid": "abc123",
-        "kty": "RSA",
-        "n": public_key.decode('utf-8'),
-        "e": "AQAB",  #Exponent (fixed value for RSA)
-        "exp": int(time.time()) + 3600  #Set an initial expiration time (1 hour)
-    }
-refresh_keys()
-
-'''
 #Sample RSA key - Delete after testing
 private_key, public_key = generate_rsa_key()
-'''
-
-
-
-
-
 
 jwks = {"keys": [sample_key]}
 
@@ -137,8 +86,10 @@ def authenticate():
 
 #----------------------------TESTING AREA
 
+#setup endpoints for
+# - post /auth, POST /auth?expired
 
-'''
+
 @app.route("/auth", methods=["GET"])
 #get put delete path head
 def testget():
@@ -148,12 +99,10 @@ def testget():
     return jsonify({"jwt_token": jwt_token})
 
 
-#setup endpoints for
-# - post /auth, POST /auth?expired
 
 
 #running test   -- possible issue with refresh keys not working correctly
-#Removed due to static use case being replaced with dynamic
+#previously Removed due to static use case being replaced with dynamic
 sample_key = {
     "kid": "abc123",
     "kty": "RSA",
